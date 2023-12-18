6755b4fc08098d9b19ccbc69590523445a119a50
Nasuha Asri <NASAS@orsted.com>
2023-12-18 14:02:11 +0800

    - add http request in angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+
+// A lifecycle hook that is called after Angular has initialized all data-bound properties of a directive
+// to  handle any additional initialization tasks.
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users: any; // declaring variable like javascript
+  
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    // add any initialization code that we want to do
+    // http is injected via the constructor method and we use this keyword
+    // returns observable object
+    this.http.get('https://localhost:5000/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request completed')
+    })
+  }
+
 }
