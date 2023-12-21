51cec885b0a48035ce815ad14c93134b58b3e6f2
Nasuha Asri <NASAS@orsted.com>
2023-12-21 14:10:37 +0800

    lesson 44: adding a token service

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API.Data;
+using API.interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,19 @@ builder.Services.AddDbContext<DataContext>(opt => {
 });
 
 builder.Services.AddCors();
+// lifetime on how long do we want the services to be available
+/*
+* 
+1. AddTransient() 
+    - short live service
+    - the token service will be created and disposed within the request as soon as it's used and finished
+    - when controller disposed at the end of the HTTP request, any dependent services also disposed
+2. AddSingleton()
+    - create service that's instantiated when application first starts
+    - never dispose until application close down
+3. AddScoped()
+*/
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
