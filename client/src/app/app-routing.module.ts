76ba33c94973956b4ee1d5c88fa0b624d509a1e8
Nasuha Asri <NASAS@orsted.com>
2023-12-26 18:20:33 +0800

    lesson 70: adding angular route guard
    - create folder _guards
    - ng g g _guards/auth --skip-tests > can activate

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'members', component: MemberListComponent },
-  { path: 'members/:id', component: MemberDetailComponent },
-  { path: 'lists', component: ListsComponent },
-  { path: 'messages', component: MessagesComponent },
+  { path: 'members', component: MemberListComponent, canActivate: [authGuard] },
+  { path: 'members/:id', component: MemberDetailComponent, canActivate: [authGuard] },
+  { path: 'lists', component: ListsComponent, canActivate: [authGuard] },
+  { path: 'messages', component: MessagesComponent, canActivate: [authGuard] },
   // if other path than abvove being entered, Home will be displayed
   // and we want path to be full match from the left
   { path: '**', component: HomeComponent, pathMatch: 'full' },
