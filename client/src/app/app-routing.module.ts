1769d8be8fc2c738206607de0953b36b2f891d21
Nasuha Asri <NASAS@orsted.com>
2023-12-27 15:08:27 +0800

    lesson 82: handling not found
    - create not found and server error component
    ---- ng g c errors/not-found --skip-tests
    ---- ng g c errors/server-error --skip-tests
    - remove error message in nav component for login method bcs we've handled it in our interceptor

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
 import { TestErrorComponent } from './errors/test-error/test-error.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -22,9 +24,11 @@ const routes: Routes = [
     ]
   },
   { path: 'errors', component: TestErrorComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: 'server-error', component: ServerErrorComponent },
   // if other path than abvove being entered, Home will be displayed
   // and we want path to be full match from the left
-  { path: '**', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -31,7 +31,6 @@ export class NavComponent {
   login() {
     this.accountService.login(this.model).subscribe({
       next: _ => this.router.navigateByUrl('/members'),
-      error: error => this.toastr.error(error.error)
     })
   }
 
