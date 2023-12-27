5d32849811d2463aba55231b8517474d53852eee
Nasuha Asri <NASAS@orsted.com>
2023-12-27 16:36:03 +0800

    lesson 88: entity framework relationship
    - we dont want a photo exists w/o a user as current migration will allow the photo to exists w/o user
    - so we use fully defined relationship to handle this
    - add relationship property in Photo entity
    ---- add-migration ExtendedUserEntity
    ---- update database
    ---- https://www.learnentityframeworkcore.com/migrations/update-database
    - the behaviour we impose is cascade delete, once user is deleted, the photo related to the user is also deleted

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,5 @@
+using API.Extensions;
+
 namespace API.Entities;
 
 public class AppUser
@@ -19,4 +21,8 @@ public class AppUser
     public string City { get; set; }
     public string Country { get; set; }
     public List<Photo> Photos { get; set; } = new List<Photo>();
+
+    public int GetAge() {
+        return DateOfBirth.CalculateAge();
+    }
 }
--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities;
 
+[Table("Photos")] // what name we want to put in database
 public class Photo
 {
     public int Id { get; set; }
     public string Url { get; set; }
     public bool IsMain { get; set; }
     public string PublicId { get; set; }
+
+    // relationship property
+    public int AppUserId { get; set; }
+    public AppUser AppUser { get; set; }
 }
