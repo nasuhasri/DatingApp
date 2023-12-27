997793350755565cb995c7c35f559ff553a5bbe8
Nasuha Asri <NASAS@orsted.com>
2023-12-27 11:42:27 +0800

    Section 7: Error Handling
    lesson 76: create error controller for testing error
    - buggycontroller: return http error response to client

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,6 +9,7 @@ namespace API.DTOs
         [Required]
         public string Username { get; set; }
         [Required]
+        [StringLength(8, MinimumLength = 3)]
         public string Password { get; set; }
     }
 }
