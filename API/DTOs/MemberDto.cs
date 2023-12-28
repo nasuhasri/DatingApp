dbcef03d64df65b220dace4dbcaea4c984ad5c40
Nasuha Asri <NASAS@orsted.com>
2023-12-28 16:17:57 +0800

    lesson 95: adding dto for members
    ---- error in lesson 94: object cycle
    - we did include() in UserRepository but got error with object cycle where we want AppUser.
    - at the end of the entity, we want a list of photos, go to Photo entity and at the end of it, we want AppUser.
    - so it is referred to object cycle
    ---- solution
    - add MemberDto (extract what we really want to return from User entity)
    - add PhotoDto

---
+++ API/DTOs/MemberDto.cs
@@ -0,0 +1,25 @@
+using System.Text.Json.Serialization;
+using API.Helper;
+
+namespace API.DTOs
+{
+    public class MemberDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        // DateOnly only display date not included time. DateTime will include time.
+        [JsonConverter(typeof(DateOnlyJsonConverter))]
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+        // always use utc which equivalent to gmt time especially when handling user from different time zones
+        public DateTime DateCreated { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<PhotoDto> Photos { get; set; }
+    }
+}
