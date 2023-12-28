d1f1e3dbb63bf991c93a9508edc3459629d6a33d
Nasuha Asri <NASAS@orsted.com>
2023-12-28 12:05:20 +0800

    lesson 90: seeding data part 1
    - create seed class
    - and seed it in program class, our starting point of the api project

---
+++ API/Data/Seed.cs
@@ -0,0 +1,39 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(DataContext context) 
+        {
+            // if there's user, we will not seed data
+            if (await context.Users.AnyAsync()) return;
+
+            var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+
+            var options = new JsonSerializerOptions{ PropertyNameCaseInsensitive = true };
+
+            // Deserialize bcs we want convert json object into C# object
+            // and speciy it as a list of user
+            // options var can be in the second parameter
+            var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+
+            foreach (var user in users) {
+                using var hmac = new HMACSHA512();
+
+                user.UserName = user.UserName.ToLower();
+                // create password for each user
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+                user.PasswordSalt = hmac.Key;
+
+                context.Users.Add(user);
+            }
+
+            await context.SaveChangesAsync();
+        }   
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,5 +24,22 @@ app.UseAuthentication(); // do you have valid token?
 app.UseAuthorization(); // user have valid token, what user is allowed to do?
 
 app.MapControllers();
+
+// give access to all the services we have inside the program class
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    // MigrateAsync(): Asynchronously applies any pending migrations for the context to the database. Will create the database if it does not already exist.
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration!");
+}
 
 app.Run();
