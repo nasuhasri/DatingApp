e5ec0b546d9b872cec57ba58a0ac9bdde6f2f4c9
Nasuha Asri <NASAS@orsted.com>
2024-01-03 11:01:59 +0800

    lesson 108: adding animate button
    - add it in member card component

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrls: ['./member-card.component.css']
+  styleUrls: ['./member-card.component.css'],
+  // Emulated is by default which means any css written only applied to this specific component
+  // encapsulation: ViewEncapsulation.Emulated
 })
 export class MemberCardComponent implements OnInit {
   @Input() member: Member | undefined;
