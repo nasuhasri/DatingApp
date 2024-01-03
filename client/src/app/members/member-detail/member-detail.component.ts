8b36cb68092e801f8afdc46e938c0518e9219033
Nasuha Asri <NASAS@orsted.com>
2024-01-03 14:59:15 +0800

    lesson 113: adding a photo gallery
    - install ng gallery
    ---- npm i ng-gallery @angular/cdk@16
    ---- https://ngx-gallery.netlify.app/#/
    - stand-alone component: allows us to create components that are not attached to ng module and they operate in stand-alone mode instead
    - Member detail component is a stand alone component that is not part of ng module, so we need to import CommonModule and TabsModule

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,15 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { Member } from 'src/app/_models/member';
 import { MemberService } from 'src/app/_service/member.service';
 
 @Component({
   selector: 'app-member-detail',
+  standalone: true,
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  imports: [CommonModule, TabsModule, GalleryModule]
 })
 export class MemberDetailComponent implements OnInit {
   member: Member | undefined;
+  images: GalleryItem[] = [];
 
   constructor(private memberService: MemberService, private route: ActivatedRoute) {}
 
@@ -24,7 +30,18 @@ export class MemberDetailComponent implements OnInit {
     if (!username) return;
 
     this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
+      next: member => {
+        this.member = member,
+        this.getImages()
+      }
     })
+  }
+
+  getImages() {
+    if (!this.member) return;
+
+    for (const photo of this.member.photos) {
+      this.images.push(new ImageItem({ src: photo.url, thumb: photo.url }));
+    }
   }
 }
