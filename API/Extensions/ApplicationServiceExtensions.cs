e1803514e577cea82acb671eb4d4678ddc082680
Nasuha Asri <NASAS@orsted.com>
2024-01-05 11:00:40 +0800

    lesson 128: configuring cloudinary in the API
    - install CloudinaryDotNet via nuget gallery (ctrl + shift + p)
    - create CloudinarySettings class
    - add it in ApplicationServiceExtensions
    - add appsettings.json to gitignore

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Helpers;
 using API.interfaces;
 using API.Interfaces;
 using API.Services;
@@ -33,6 +34,7 @@ namespace API.Extensions
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 
             return services;
         }
---
+++ API/Helpers/CloudinarySettings.cs
@@ -0,0 +1,10 @@
+namespace API.Helpers
+{
+    public class CloudinarySettings
+    {
+        // properties must be same name as CloudinarySettings in json file
+        public string CloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
