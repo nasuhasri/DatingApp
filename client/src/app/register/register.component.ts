ad824d85fdd2371134d483656ccc2efb2db8b676
Nasuha Asri <NASAS@orsted.com>
2024-01-08 11:53:19 +0800

    lesson 145: adding custom validators
    - add function matchValues to compare password and confirmPassword control which return control type

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -25,8 +25,21 @@ export class RegisterComponent implements OnInit {
     this.registerForm = new FormGroup({
       username: new FormControl('Hello', Validators.required),
       password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', Validators.required),
-    })
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
+    });
+
+    // to ensure both password and confirm password matches
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      // comparing one password control to another password control
+      // if values match, return null -> validated
+      return control.value === control.parent?.get(matchTo)?.value ? null : { notMatching: true }
+    }
   }
 
   register() {
