fe4b06fc078be1fe0a0f3530cd73a396c7abc369
Nasuha Asri <NASAS@orsted.com>
2024-01-08 11:37:30 +0800

    Section 12: Reactive Forms
    Lesson 143: Reactive form introduction
    - reactive forms are component based
    - we create and control the form via component instead of the template
    - easier to use and control validation we are using

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,19 +12,31 @@ import { ToastrService } from 'ngx-toastr';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter(); // child to parent component
   model: any = {}
+  // Tracks the value and validity state of a group of FormControl instances
+  registerForm: FormGroup = new FormGroup({});
 
   constructor(private accountService: AccountService, private toastr: ToastrService) {}
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
+    })
   }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: () => {
-        this.cancel();
-      },
-      error: error => this.toastr.error(error.error)
-    })
+    console.log(this.registerForm?.value);
+    // this.accountService.register(this.model).subscribe({
+    //   next: () => {
+    //     this.cancel();
+    //   },
+    //   error: error => this.toastr.error(error.error)
+    // })
   }
 
   cancel() {
