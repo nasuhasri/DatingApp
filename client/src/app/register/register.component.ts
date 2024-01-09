72d6dd1da3d44fdce178e3f71a60ceddfc932c51
Nasuha Asri <NASAS@orsted.com>
2024-01-09 14:43:48 +0800

    lesson 150: adding reusable date input
    - using bsdatepicker from ngx bootstrap
    - create date picker component
    ---- ng g c _forms/date-picker --skip-tests
    - Partial keyword in angular bootsrap means optional

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -14,11 +14,14 @@ export class RegisterComponent implements OnInit {
   model: any = {}
   // Tracks the value and validity state of a group of FormControl instances
   registerForm: FormGroup = new FormGroup({});
+  maxDate: Date = new Date();
 
   constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.initializeForm();
+    // pass to date picker and only allow us to select up to max date
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
   initializeForm() {
