8e4104f4e8767de7787282f6403fd2e36ac59aff
Nasuha Asri <NASAS@orsted.com>
2024-01-13 16:00:02 +0800

    lesson 166: Making the Last Active action filter more optimal
    - when we update lastActive property, the sql is too complex even though we only getting one user.
    - we use TokenService to provide us with the id and username so can get a hold of GetUserByIdAsync() in userRepository

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -28,7 +28,8 @@ namespace API.Services
             // claim - a bit of information that a user claims to be his/her
             var claims = new List<Claim> 
             {
-                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+                new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName)
             };
 
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
