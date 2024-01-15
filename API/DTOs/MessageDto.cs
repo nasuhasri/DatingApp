de075a2c85c648b7e54dfd819aed6d2a88951cdc
Nasuha Asri <NASAS@orsted.com>
2024-01-15 15:07:38 +0800

    lesson 185: Setting up the message repository
    - create interface when creating new repository or service
    - create MessageRepository
    - create MessageDto

---
+++ API/DTOs/MessageDto.cs
@@ -0,0 +1,19 @@
+using API.Entities;
+
+namespace API.DTOs
+{
+    public class MessageDto
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public string SenderUsername { get; set; }
+        public string SenderPhotoUrl { get; set; }
+        public int ReceipentId { get; set; }
+        public string ReceipentUsername { get; set; }
+        public string ReceipentPhotoUrl { get; set; }
+
+        public string Content { get; set; }
+        public DateTime? DateRead { get; set; }
+        public DateTime MessageSent { get; set; }
+    }
+}
