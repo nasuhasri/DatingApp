d56bf5e8c20c4d313d63bb614d17e89a125b8c09
Nasuha Asri <NASAS@orsted.com>
2024-01-19 23:06:51 +0800

    lesson 218: Setting up modals
    - import modal module from ngx bootstrap
    - create new roles-modal component
    ---- ng g c modals/roles-modal --skip-tests
    - to populate properties inside roles modal component from user management component
    - create modal with initial properties

---
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-roles-modal',
+  templateUrl: './roles-modal.component.html',
+  styleUrls: ['./roles-modal.component.css']
+})
+export class RolesModalComponent implements OnInit {
+  // pass these properties to roles modal component
+  title = '';
+  list: any;
+  closeBtnName = '';
+
+  constructor(public bsModalRef: BsModalRef) {}
+
+  ngOnInit(): void {
+    
+  }
+}
