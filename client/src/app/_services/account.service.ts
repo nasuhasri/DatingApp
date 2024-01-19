936d46f15b07d82953a3b20d3dd3934b824616df
Nasuha Asri <NASAS@orsted.com>
2024-01-19 17:31:04 +0800

    lesson 215: Adding an admin guard
    - add admin guard
    ---- ng g guard _guards/admin --skip-tests
    ---- can activate route
    - the admin component can only be accessed if user pass the authGuard and adminGuard

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -44,6 +44,10 @@ export class AccountService {
   }
 
   setCurrentUser(user: User) {
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
+
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
@@ -51,5 +55,10 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+  }
+
+  getDecodedToken(token: string) {
+    // return the second part of the token (can decode token via jwt.ms)
+    return JSON.parse(atob(token.split(".")[1]))
   }
 }
