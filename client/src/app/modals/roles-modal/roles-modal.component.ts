9ebf4e6996bda4dcc8cd650f2e267613551862f1
Nasuha Asri <NASAS@orsted.com>
2024-01-19 23:20:17 +0800

    lesson 219: editing roles in the client
    - modify modal code to use the property related to application

--- client/src/app/modals/roles-modal/roles-modal.component.ts
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -8,13 +8,19 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
 })
 export class RolesModalComponent implements OnInit {
   // pass these properties to roles modal component
-  title = '';
-  list: any;
-  closeBtnName = '';
+  username = '';
+  availableRoles: any[] = [];
+  selectedRoles: any[] = [];
 
   constructor(public bsModalRef: BsModalRef) {}
 
   ngOnInit(): void {
     
+  }
+
+  // change values if check box is checked and vice versa
+  updateChecked(checkedValue: string) {
+    const index = this.selectedRoles.indexOf(checkedValue);
+    index !== -1 ? this.selectedRoles.splice(index, 1) : this.selectedRoles.push(checkedValue);
   }
 }
